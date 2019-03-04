710e1c399bd3f885b13cf56994eafe943e741815
AgustinNG <AgustinNavarro@cecotec.es>
2019-03-04 09:47:22 +0100

    Mostrar resultado final

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class Resultado extends Component {
     render() {
+        const resultado = this.props.resultado;
+        const mensaje = (!resultado) ? 'Elije Marca, año y Tipo de Seguro' : 'El total es $'
         return (
-            <p>{this.props.resultado} €</p>
+            <div className="gran-total">
+                {mensaje}
+                <TransitionGroup component="span" className="resultado">
+                    <CSSTransition className="resultado" timeout={{start:500, exit: 500}}>
+                        <span>{resultado}</span>
+                    </CSSTransition>
+                </TransitionGroup>
+            </div>
         )
     }
 }
